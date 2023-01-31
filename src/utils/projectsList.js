e286d942fdd37cbf4fbaf48fb8ce51778d367ff5
MrLearn <learnmr67@gmail.com>
2023-01-31 20:19:28 +0300

    обновил/удалил содержимое

---
+++ src/utils/projectsList.js
@@ -0,0 +1,61 @@
+import project01 from "./../assets/projects/01.jpg";
+import project01Big from "./../assets/projects/01-big.jpg";
+
+import project02 from "./../assets/projects/02.jpg";
+import project02Big from "./../assets/projects/02-big.jpg";
+
+import project03 from "./../assets/projects/03.jpg";
+import project03Big from "./../assets/projects/03-big.jpg";
+
+import project04 from "./../assets/projects/04.jpg";
+import project04Big from "./../assets/projects/04-big.jpg";
+
+import project05 from "./../assets/projects/05.jpg";
+import project05Big from "./../assets/projects/05-big.jpg";
+
+import project06 from "./../assets/projects/06.jpg";
+import project06Big from "./../assets/projects/06-big.jpg";
+
+const projects = [
+	{
+		title: 'Gaming streaming portal',
+		skills: 'React, Node.js, MongoDB',
+		img: project01,
+		imgBig: project01Big,
+		gitHubLink: 'https://github.com',
+	},
+	{
+		title: 'Video service',
+		img: project02,
+		imgBig: project02Big,
+		skills: 'React, PHP, MySql',
+		gitHubLink: 'https://github.com',
+	},
+	{
+		title: 'Video portal',
+		img: project03,
+		imgBig: project03Big,
+		skills: 'Vue JS, Node.js, MongoDB',
+		gitHubLink: 'https://github.com',
+	},
+	{
+		title: 'Dating app',
+		img: project04,
+		imgBig: project04Big,
+		skills: 'React Native',
+	},
+	{
+		title: 'Landing',
+		img: project05,
+		imgBig: project05Big,
+		skills: 'HTML, SCSS, JS',
+	},
+	{
+		title: 'Gaming community',
+		img: project06,
+		imgBig: project06Big,
+		skills: 'React, PHP, MySql',
+	},
+];
+
+export {projects}
